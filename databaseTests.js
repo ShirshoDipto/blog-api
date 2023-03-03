548b15e5b033b2146f0b5a1efeb2926b11f5d335
Shirsho Dipto <shirshodipto@gmail.com>
2023-03-03 20:04:15 +0600

    Test models and schemas

--- databaseTests.js
+++ databaseTests.js
@@ -1,4 +1,9 @@
 const Post = require("./models/post")
+const Comment = require("./models/comment")
+const Reply = require("./models/reply")
+const comment = require("./models/comment")
+const CommentLike = require("./models/commentLike")
+const PostLike = require("./models/postLike")
 const mongoose = require("mongoose")
 require("dotenv").config()
 
@@ -7,26 +12,48 @@ const mongodb = process.env.MONGODB_URI
 async function main() {
   await mongoose.connect(mongodb)
   console.log("Connection Successful. ")
+// an id "640189b3c2dc3b9e1a4b2446"
+  // const newReply = new Reply({
+  //   author: {
+  //     authorId: "640189b3c2dc3b9e1a4b2446",
+  //     firstName: "Shusme",
+  //     lastName: "Islam"
+  //   },
+  //   content: "Hello amr jaan",
+  //   numLikes: "5000",
+  // })
 
-  const newPost = new Post({
-    title: "hello",
-    content: "Text content",
-    author: {
-    author1: {
-      authorId: "63f6e1a67b38768ca361679f",
-      firstName: "Shirsho",
-      lastName: "Dipto"
-    },
-    author2: {
-      authorId: "63f6e1a67b38768ca361679f",
-      firstName: "Shirsho",
-      lastName: "Dipto"
-    },
-  }
+  // const savedReply = await newReply.save()
+
+  // const newComment = new Comment({
+  //   content: "Hello",
+  //   author: {
+  //     authorId: "640189b3c2dc3b9e1a4b2446",
+  //     firstName: "Shirsho",
+  //     lastName: "Dipto"
+  //   },
+  //   replies: [savedReply._id],
+  //   numLikes: "1000",
+  //   postId: "640189b3c2dc3b9e1a4b2446"
+  // })
+
+  // const savedComment = await newComment.save()
+  // const comment = await Comment.findOne({}).populate("replies")
+  // console.log(comment.replies[0].date_formatted)
+
+  const newCommentLike = new CommentLike({
+    author: "640189b3c2dc3b9e1a4b2446",
   })
 
-  const savedPost = await newPost.save()
-  console.log(savedPost)
+  const savedLikeComment = await newCommentLike.save()
+  console.log("comment like", savedLikeComment)
+
+  const newPostLike = new PostLike({
+    author: "640189b3c2dc3b9e1a4b2446",
+  })
+
+  const savedLikePost = await newPostLike.save()
+  console.log("post like", savedLikePost)
 }
 
 main().catch(err => console.log(err))
--- models/reply.js
+++ models/reply.js
@@ -3,23 +3,21 @@ const { DateTime } = require("luxon")
 
 const Schema = mongoose.Schema
 
-const CommentSchema = new Schema({
-  content: {type: String, required: true},
+const ReplySchema = new Schema({
   author: {
     authorId: {type: Schema.Types.ObjectId, ref:"User", requied: true},
     firstName: {type: String, required: true}, 
     lastName: {type: String, required: true}
   },
-  date: {type: Date, default: Date.now},
-  replies: [{type: Schema.Types.ObjectId, ref: "Reply"}],
-  postId: {type: Schema.Types.ObjectId, requied: true, ref: "Post"},
-  numLikes: {type: String, required: true}
+  content: {type: String, required: true},
+  numLikes: {type: String, required: true},
+  date: {type: Date, default: Date.now}
 })
 
-CommentSchema.virtual("date_formatted").get(function () {
+ReplySchema.virtual("date_formatted").get(function () {
   return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
 });
 
 // There will be an url virtual as well. 
 
-module.exports = mongoose.model("Comment", CommentSchema)
+module.exports = mongoose.model("Reply", ReplySchema)
